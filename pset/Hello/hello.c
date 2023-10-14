cb0d0184e6cbb0c9166a1e514c4133e4bded3bb9
Glaubert01 <124006122+Glaubert01@users.noreply.github.com>
2023-10-14 12:28:42 -0300

    Update hello.c

--- pset/Hello/hello.c
+++ pset/Hello/hello.c
@@ -1,1 +1,11 @@
+#include <cs50.h>  // Adiciona a biblioteca do CS50
+#include <stdio.h>  // Adiciona a biblioteca standard
 
+int main(void)
+{
+
+    // get_string é uma função específica da biblioteca do CS50!
+
+    string nome = get_string("Qual é seu nome? \n");
+    printf("Olá, %s \n", nome);
+}
