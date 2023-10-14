5addd95ab1f96c7617bd72923b7d52eb9271842f
Glaubert01 <124006122+Glaubert01@users.noreply.github.com>
2023-10-14 12:44:02 -0300

    Create mario_hard.c

---
+++ pset/mario/mario_hard.c
@@ -0,0 +1,26 @@
+#include <cs50.h>  // Adds CS50 library
+#include <stdio.h>  // Adds the Standard library
+
+int i, j, n;  // Declaration of variables
+
+int main(void)
+{
+    do
+    {
+        n = get_int("Enter an integer between 1 and 8: ");  // Get a number from user between a limite
+    }
+    while(n < 1 || n > 8);  // Test if the number that the user typed is between the expected limits
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = n - i; j >= 1; j--) // print the spaces
+            printf(" ");
+        for (j = 1; j <= i; j++) // print the #
+            printf("#");
+        for (j = n - 6; j >= 1; j--) // print the spaces
+            printf(" ");
+        for (j = 1; j <= i; j++) // print the #
+            printf("#");
+        printf("\n");
+    }
+}
